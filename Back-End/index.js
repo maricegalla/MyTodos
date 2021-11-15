7b392a2fc72ef475588df9592f1378284f694ce9
maricegalla <maricegalla@gmail.com>
2021-11-14 21:49:07 -0300

    updated port

--- Back-End/index.js
+++ Back-End/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = '3001';
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`💃🏻Mãe tá on na porta ${PORT}!🤘🏻`);
